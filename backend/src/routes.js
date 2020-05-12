320f1ab98f7a104a7954c7b25242e9bdcc2eb28b
Rafael Lopes de Melo <rafaelmelo.programmer@gmail.com>
2020-05-11 21:38:09 -0300

    Add GetPDF function

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -3,8 +3,13 @@ const router = express.Router();
 const puppeteer = require('puppeteer');
                                     
 router.get('/', async (req, res) => {
+   await getPDF();
+})
+
+async function getPDF() {
     const browser = await puppeteer.launch({headless: false});
-    const page = await browser.newPage();
+    let page = await browser.newPage();
+
     await page.goto('https://pje.trt2.jus.br/primeirograu/login.seam?cid=515669');
     await page.type('#username', '09369008810');
     await page.type('#password', 'pje9963');
@@ -15,6 +20,24 @@ router.get('/', async (req, res) => {
     await page.keyboard.press('Home');
     await page.keyboard.type('00000524920140252');
     await page.click('input[name=consultaProcessoAdvogadoForm\\:searchButon]');
-})
+    await page.waitFor(2000);
+    await page.click('#leftAdvPnl_header_label');
+    for(i=0; i<3; i++){
+        await page.keyboard.press('Tab');
+    }
+    await page.keyboard.press('Enter');
+
+    await page.waitFor(1000);
+
+    let pages = await browser.pages();
+    page = pages[2];
+
+    await page.click('a img')
+    await page.waitFor(1000);
+    for(i=0; i<11; i++){
+        await page.keyboard.press('Tab');
+    }
+    await page.keyboard.press('Enter');
+}
 
 module.exports = router;
