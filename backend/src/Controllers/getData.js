a0c39eb8e3d2d6cab3179dabc5e2c5327b259357
Rafael Lopes de Melo <rafaelmelo.programmer@gmail.com>
2020-05-13 16:52:03 -0300

    Missing clean data's array

--- backend/src/Controllers/getData.js
+++ backend/src/Controllers/getData.js
@@ -1,15 +1,20 @@
-const puppeteer = require('puppeteer');
 const fs = require('fs');
 const pdf = require('pdf-parse');
 const downloadsFolder = require('downloads-folder');
 const downloadsPath = downloadsFolder();
+let data;
 
 const processoNumero = '1000452-49.2013.5.02.0521';
 const pdfPath = `${downloadsPath}/${processoNumero}.pdf`;
 
 async function getData(req, res) {
-    const dataBuffer = fs.readFileSync(pdfPath);
-    pdf(dataBuffer, {max: 1}).then(data => res.json(data.text.split('\n')));
+    const dataBuffer = await fs.readFileSync(pdfPath);
+    await pdf(dataBuffer, {max: 1})
+    .then(result => data = result.text.split(' ').join('').split('\n'));
+    const filteredData = data.filter(el => el.trim() !== "");
+    filteredData.splice(0, 3);
+    filteredData.splice(2, 1);
+    res.json(filteredData);
 }
 
 async function verifyFileExists(req, res) {
