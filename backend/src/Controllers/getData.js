07460bb386eb581cc1e7c5174e90fb6aaa294265
Rafael Lopes de Melo <rafaelmelo.programmer@gmail.com>
2020-05-14 16:29:52 -0300

    missing search

--- backend/src/Controllers/getData.js
+++ backend/src/Controllers/getData.js
@@ -2,8 +2,6 @@ const fs = require('fs');
 const pdf = require('pdf-parse');
 const downloadsFolder = require('downloads-folder');
 const downloadsPath = downloadsFolder();
-
-let filteredDataArray;
 const processoNumero = '1000452-49.2013.5.02.0521';
 const pdfPath = `${downloadsPath}/${processoNumero}.pdf`;
 
@@ -12,7 +10,14 @@ async function getData(req, res) {
     const dataBuffer = await fs.readFileSync(pdfPath);
     await pdf(dataBuffer, {max: 1})
     .then(result => dataArray = result.text.split(' ').join('').split('\n'));
-    filteredDataArray = dataArray.filter(el => el.trim() !== "")
+    let filteredDataArray = dataArray.filter(el => el.trim() !== "")
+
+    function search(term) {
+        for (i=0; i<filteredDataArray.lenght; i++) {
+            var position = filteredDataArray[i].indexOf(term);
+        }
+        return position
+    }
 
     const data = {
         author: search('RECLAMANTE'),
@@ -25,12 +30,7 @@ async function getData(req, res) {
     res.json({data, filteredDataArray});
 }
 
-function search(term) {
-    for (i=0; i<filteredDataArray.lenght; i++) {
-        var position = filteredDataArray[i].indexOf(term);
-    }
-    return position
-}
+
 
 async function verifyFileExists(req, res) {
     fs.stat(pdfPath, (err) => {
