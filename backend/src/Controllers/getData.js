6d60844407e1da1176b61ddc62f38a1b978a0479
Rafael Lopes de Melo <rafaelmelo.programmer@gmail.com>
2020-05-14 16:22:10 -0300

    Missing search

--- backend/src/Controllers/getData.js
+++ backend/src/Controllers/getData.js
@@ -2,19 +2,34 @@ const fs = require('fs');
 const pdf = require('pdf-parse');
 const downloadsFolder = require('downloads-folder');
 const downloadsPath = downloadsFolder();
-let data;
 
+let filteredDataArray;
 const processoNumero = '1000452-49.2013.5.02.0521';
 const pdfPath = `${downloadsPath}/${processoNumero}.pdf`;
 
 async function getData(req, res) {
+    
     const dataBuffer = await fs.readFileSync(pdfPath);
     await pdf(dataBuffer, {max: 1})
-    .then(result => data = result.text.split(' ').join('').split('\n'));
-    const filteredData = data.filter(el => el.trim() !== "");
-    filteredData.splice(0, 3);
-    filteredData.splice(2, 1);
-    res.json(filteredData);
+    .then(result => dataArray = result.text.split(' ').join('').split('\n'));
+    filteredDataArray = dataArray.filter(el => el.trim() !== "")
+
+    const data = {
+        author: search('RECLAMANTE'),
+        defendent: "",
+        processNumber: "",
+        court: "",
+        classification: ""
+    }
+    
+    res.json({data, filteredDataArray});
+}
+
+function search(term) {
+    for (i=0; i<filteredDataArray.lenght; i++) {
+        var position = filteredDataArray[i].indexOf(term);
+    }
+    return position
 }
 
 async function verifyFileExists(req, res) {
